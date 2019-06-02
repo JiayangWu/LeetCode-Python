6a4d8719e22e7bf30868aa39a10537f9c7b4babc
Celia_Wu <530081999@qq.com>
2019-06-02 23:27:59 +0800

    2019-06-02

--- 0010.正则表达式匹配/0010-正则表达式匹配.py
+++ 0010.正则表达式匹配/0010-正则表达式匹配.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        if not p: #如果p为空，s不空，return false， s空, return true
+        if not p:
             return not s
-        
         match = s and p[0] in [s[0], "."]
         if len(p) > 1 and p[1] == "*":
-            return self.isMatch(s, p[2:]) or (match and self.isMatch(s[1:], p))
+            return self.isMatch(s, p[2:]) or (match and  self.isMatch(s[1:], p))
         return match and self.isMatch(s[1:], p[1:])
+        
