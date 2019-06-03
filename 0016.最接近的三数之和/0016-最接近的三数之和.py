0524c014c3fbeacf46935d2eef36cd869dc1cf16
Celia_Wu <530081999@qq.com>
2019-06-03 22:44:35 +0800

    2019-06-03

--- 0016.最接近的三数之和/0016-最接近的三数之和.py
+++ 0016.最接近的三数之和/0016-最接近的三数之和.py
@@ -6,22 +6,19 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        res = nums[0] + nums[1] + nums[2]
+        res = 99999999
         for i, num in enumerate(nums):
             left, right = i + 1, len(nums) - 1
-            
-            while left < right:
+            while(left < right):
                 s = num + nums[left] + nums[right]
-                # print s, res, abs(s - target), abs(res - target)
+                
                 if abs(s - target) < abs(res - target):
                     res = s
+                
                 if s == target:
                     return s
-                elif s < target:
-                    left += 1
-                else:
+                elif s > target:
                     right -= 1
+                else:
+                    left += 1
         return res
-                
-            
-        
