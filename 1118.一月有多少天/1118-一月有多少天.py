054360fa91599a209d24404c4867af4cf88588d4
Celia_Wu <530081999@qq.com>
2019-08-15 22:32:13 -0400

    2019-08-15

--- 1118.一月有多少天/1118-一月有多少天.py
+++ 1118.一月有多少天/1118-一月有多少天.py
@@ -6,8 +6,7 @@ class Solution(object):
         :rtype: int
         """
         
-        if  (Y % 100 != 0 and Y % 4 ==0) or (Y % 100 == 0 and Y % 400 == 0) :
+        if  (Y % 100 != 0 and Y % 4 ==0) or Y % 400 == 0:
             return [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][M - 1]
         else:
             return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][M - 1]
-        
