2a8d3c1a26fda47c5e6823fc42aa00e0bd600bf5
Celia_Wu <530081999@qq.com>
2019-09-08 21:01:45 -0400

    2019-09-08

--- 0290.单词规律/0290-单词规律.py
+++ 0290.单词规律/0290-单词规律.py
@@ -1,29 +1,20 @@
 class Solution(object):
-    def wordPattern(self, pattern, str):
+    def wordPattern(self, pattern, s):
         """
         :type pattern: str
         :type str: str
         :rtype: bool
         """
-        s = str.split(" ")
+        s = s.split(" ")
         if len(pattern) != len(s):
             return False
-        record = [0 for i in range(0, 26)] #记录下pattern里每个字母出现的次数
-        hashmap = dict()
-        
-        for i, word in enumerate(s):
-            t = ord(pattern[i]) - ord("a")
-            
-            if word not in hashmap.keys():
-                if record[t] > 0:
-                    return False
-                hashmap[word] = pattern[i]
-                record[t] = 1
+        dic = {}
+        for i, char in enumerate(pattern):
+            if char not in dic:
+                dic[char] = s[i]
             else:
-                if hashmap[word] != pattern[i]:
+                if dic[char] != s[i]:
                     return False
+
+        return len(set(dic.values())) == len(dic.values())
                 
-        return True
-                
-            
-        
--- 0293.翻转游戏/0293-翻转游戏.py
+++ 0293.翻转游戏/0293-翻转游戏.py
@@ -6,6 +6,6 @@ class Solution(object):
         """
         res = []
         for i in range(len(s) - 1):
-            if s[i:i+2] == "++":
-                res.append(s[:i] + "--" + s[i+2:])
+            if s[i:i + 2] == "++":
+                res.append(s[:i] + "--" + s[i + 2:])
         return res
