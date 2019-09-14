540a28ddbe47fb344fdf838348f917183878c1d5
Celia_Wu <530081999@qq.com>
2019-09-13 22:41:44 -0400

    2019-09-13

--- 0295.数据流的中位数/0295-数据流的中位数.py
+++ 0295.数据流的中位数/0295-数据流的中位数.py
@@ -1,41 +1,42 @@
 from heapq import *
+# class Heaps(object):
+#     def __init__(self):
+#         self.min_heap = []
+#         self.max_heap = []
+        
+#     def __
 class MedianFinder(object):
-# 维护两个堆，一个大顶堆，一个小顶堆，小顶堆里的数比大顶堆里的数都要大， 
-# 如果有两个潜在的中位数（两个堆size相同），数据流的中位数就是两个堆顶之和除以2
-# 如果只有一个中位数，就看size更小的那个堆的堆顶
-# 如果新进来的数比小顶堆的数要小，就把它插入大顶堆
-# 如果新进来的数比小顶堆的数要大，就把它插入小顶堆
-# 调整两个堆，使得size 差最大为1
+
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.max_h = list()
-        self.min_h = list()
-        heapify(self.max_h)
-        heapify(self.min_h)
-        
+        self.min_heap = []
+        self.max_heap = []
+        heapify(self.min_heap)
+        heapify(self.max_heap)
 
     def addNum(self, num):
         """
         :type num: int
         :rtype: None
         """
-        heappush(self.min_h, num)
-        heappush(self.max_h, -heappop(self.min_h))
-        if len(self.max_h) > len(self.min_h):
-            heappush(self.min_h, -heappop(self.max_h))
+        heappush(self.min_heap, num)
+        heappush(self.max_heap, -heappop(self.min_heap))
+        if len(self.max_heap) > len(self.min_heap):
+            heappush(self.min_heap, -heappop(self.max_heap))
+        
 
     def findMedian(self):
         """
         :rtype: float
         """
-        max_len = len(self.max_h)
-        min_len = len(self.min_h)
-        if max_len == min_len: #有两个候选中位数
-            return (self.min_h[0] + -self.max_h[0]) / 2.
-        else:#小顶堆的size 一定 >= 大顶堆的size，所以答案就是小顶堆的堆顶
-            return self.min_h[0] / 1.
+        l_min_heap = len(self.min_heap)
+        l_max_heap = len(self.max_heap)
+        if l_min_heap == l_max_heap:
+            return (self.min_heap[0] - self.max_heap[0]) /2.
+        else:
+            return self.min_heap[0]/1.
             
         
 
