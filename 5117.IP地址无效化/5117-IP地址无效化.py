ea296fdc7188d64b331e4d69c9679ee60c28d346
Jiayang Wu <jiayangwu1013@gmail.com>
2019-11-18 21:01:37 -0500

    delete

--- 5117.IP地址无效化/5117-IP地址无效化.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def defangIPaddr(self, address):
-        """
-        :type address: str
-        :rtype: str
-        """
-        address = address.replace(".", "[.]")
-        return address
