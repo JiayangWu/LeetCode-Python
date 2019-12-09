8dbdc4897aa80776d24442a2aaba4db233ecf0ff
JiayangWu <530081999@qq.com>
2019-12-09 10:31:05 -0500

    2019-12-09

--- 1277.统计全为1的正方形子矩阵/1277-统计全为1的正方形子矩阵.py
+++ 1277.统计全为1的正方形子矩阵/1277-统计全为1的正方形子矩阵.py
@@ -4,22 +4,26 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: int
         """
+        if not matrix or not matrix[0]:
+            return 0
+        
         m, n = len(matrix), len(matrix[0])
-        res = 0
         dp = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            dp[i][0] = matrix[i][0]
-            res += dp[i][0]
-            
-        for j in range(1, n):
-            dp[0][j] = matrix[0][j]
-            res += dp[0][j]
-            
+        
+        res = 0
+        for j in range(n):
+            if matrix[0][j]:
+                res += 1
+                dp[0][j] = 1
+        
+        for i in range(1, m):
+            if matrix[i][0]:
+                res += 1
+                dp[i][0] = 1
+                
         for i in range(1, m):
             for j in range(1, n):
                 if matrix[i][j]:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
                     res += dp[i][j]
-        print dp
         return res
-            
