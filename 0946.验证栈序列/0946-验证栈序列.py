845c31dda2d05bf966fbfe26516375ab26d3ff00
JiayangWu <530081999@qq.com>
2020-02-12 23:47:18 -0500

    2020-02-12

--- 0946.验证栈序列/0946-验证栈序列.py
+++ 0946.验证栈序列/0946-验证栈序列.py
@@ -5,51 +5,12 @@ class Solution(object):
         :type popped: List[int]
         :rtype: bool
         """
+        s = []
+        popped = popped[::-1]
+        for num in pushed:
+            s.append(num)
+            while s and popped and s[-1] == popped[-1]:
+                s.pop()
+                popped.pop()
         
-        stack = []
-        i = 0
-        for item in pushed:
-            stack.append(item)
-            while(stack and popped[i] == stack[-1]):
-                stack.pop()
-                i += 1
-        return stack == []
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        l = len(pushed)
-        
-        stack = list()
-
-        
-        for i in range(0, l):
-            stack.append(pushed[i])
-            while(stack and stack[-1] == popped[0]):
-                stack = stack[:-1]
-                popped = popped[1:]
-
-        return stack == []
-
-                
-                
-        
+        return not s and not popped
