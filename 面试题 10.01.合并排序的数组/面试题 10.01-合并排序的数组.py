4f7083268383beaecd54d495ac9f5fba1e840096
JiayangWu <530081999@qq.com>
2020-03-03 14:09:24 -0500

    2020-03-03

---
+++ 面试题 10.01.合并排序的数组/面试题 10.01-合并排序的数组.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def merge(self, A, m, B, n):
+        """
+        :type A: List[int]
+        :type m: int
+        :type B: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify A in-place instead.
+        """
+        A[:] = sorted(A[:m] + B)
