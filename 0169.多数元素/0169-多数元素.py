d7030e823a1dc9aa81be19fd6518982c4dfaf912
JiayangWu <530081999@qq.com>
2020-03-12 23:35:26 -0400

    2020-03-12

---
+++ 0169.多数元素/0169-多数元素.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[len(nums) // 2]
