990ebddf9304d0f9c84af9665a1578d3870174f8
JiayangWu <530081999@qq.com>
2020-03-13 23:28:54 -0400

    2020-03-13

--- 0300.最长上升子序列/0300-最长上升子序列.py
+++ 0300.最长上升子序列/0300-最长上升子序列.py
@@ -4,22 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l = len(nums)
-        if not l:
-            return 0
-        dp = [1 for _ in range(l)]
+        dp = [1 for _ in nums]
         
-        for index, item in enumerate(nums):
-            dp[index] = self.find(nums[:index + 1], dp) + 1
-        # print dp       
-        return max(dp)
-            
-    
-    def find(self, nums, dp):
-        max_element = -1 * 2 << 31
-        
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < nums[-1]:
-                max_element = max(max_element, dp[i]) 
-
-        return max_element if max_element != -1 * 2 << 31 else 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    
+        return max(dp) if dp else 0
