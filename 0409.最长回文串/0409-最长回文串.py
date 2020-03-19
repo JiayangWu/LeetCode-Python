a018d24a92412a680e0ba813a5dc2af7816ac3fa
JiayangWu <530081999@qq.com>
2020-03-18 23:58:19 -0400

    2020-03-18

--- 0409.最长回文串/0409-最长回文串.py
+++ 0409.最长回文串/0409-最长回文串.py
@@ -4,16 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        record = [0 for i in range(0, 129)]
-        for char in s:
-            record[ord(char)] += 1
-        
-        res, flag = 0, 0
-        for i, x in enumerate(record):
-            if x % 2 == 1:
-                res += x - 1
-                flag = 1
-            else:
-                res += x
-        # print res        
-        return res + flag
+        return len(s) -max(0,sum([s.count(i)%2 for i in set(s)])-1)
