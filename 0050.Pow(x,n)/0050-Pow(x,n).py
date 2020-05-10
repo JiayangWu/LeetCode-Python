279f90dcdcf048e786393e2f998a2fb2039a94de
JiayangWu <530081999@qq.com>
2020-05-10 13:08:45 -0400

    2020-05-10

--- 0050.Pow(x,n)/0050-Pow(x,n).py
+++ 0050.Pow(x,n)/0050-Pow(x,n).py
@@ -1,22 +1,8 @@
 class Solution(object):
-    def myPow(self, x0, n):
+    def myPow(self, x, n):
         """
         :type x: float
         :type n: int
         :rtype: float
         """
-        if not n:
-            return 1
-        def helper(x, n, tmp):
-            print n, x
-            if n <= 1:
-                return x * tmp
-            if n % 2:
-                tmp *= x
-                n -= 1
-            return helper(x * x, n // 2, tmp)
-        op = 1
-        if n < 0:
-            op = -1 
-        res = helper(x0, abs(n), 1)
-        return res if op > 0 else 1.0/res
+        return x ** n
