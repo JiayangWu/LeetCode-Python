90d27e4a8963c09848a9998dda6a80ce3c475bb9
JiayangWu <530081999@qq.com>
2020-07-11 21:46:09 -0400

    remote dups again

--- 1.两数之和/1-两数之和.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for index, item in enumerate(nums):
-            if target - item in hashmap:
-                return hashmap[target-item],index
-            hashmap[item] = index
