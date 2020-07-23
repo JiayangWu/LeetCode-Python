2a11d53516111bb9cf55bd30caa284b39e0be65f
JiayangWu <530081999@qq.com>
2020-07-22 23:01:46 -0400

    2020-07-22

--- 0064.最小路径和/0064-最小路径和.py
+++ 0064.最小路径和/0064-最小路径和.py
@@ -7,20 +7,15 @@ class Solution(object):
         if not grid or not grid[0]:
             return 0
         m, n = len(grid), len(grid[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                # print i, j
-                dp[i][j] = grid[i][j]
         
-        
-        dp[0][0] = grid[0][0]
-        for i in range(m):
-            for j in range(n):
-                if i - 1 >= 0 and j - 1 >= 0:
-                    dp[i][j] += min(dp[i - 1][j], dp[i][j - 1])
-                elif i - 1 >= 0:
-                    dp[i][j] += dp[i - 1][j]
-                elif j - 1 >= 0:
-                    dp[i][j] += dp[i][j - 1]
-        return dp[-1][-1]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j - 1]
+            
+        for i in range(1, m):
+            grid[i][0] += grid[i - 1][0]
+            
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+                
+        return grid[-1][-1]
