0f41ca89dc023250f72e73dca9b71bf6ecbf5c90
JiayangWu <530081999@qq.com>
2020-08-06 23:46:09 -0400

    2020-08-06

--- 0100.相同的树/0100-相同的树.py
+++ 0100.相同的树/0100-相同的树.py
@@ -1,10 +1,9 @@
 # Definition for a binary tree node.
 # class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -12,8 +11,8 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if not p:
-            return not q
-        if not q:
-            return not p
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
