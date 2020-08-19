63e65b17fbe7a57c24b2ec054ab36c43ce1e5e1c
JiayangWu <530081999@qq.com>
2020-08-19 01:19:52 -0400

    2020-08-19

--- 0860.柠檬水找零/0860-柠檬水找零.py
+++ 0860.柠檬水找零/0860-柠檬水找零.py
@@ -4,18 +4,22 @@ class Solution(object):
         :type bills: List[int]
         :rtype: bool
         """
-        five = ten = 0
-        for num in bills:
-            if num == 5:
-                five += 1
-            elif num == 10 and five:
-                ten += 1
-                five -= 1
-            elif num == 20 and five and ten:
-                five -= 1
-                ten -= 1
-            elif num == 20 and five >= 3:
-                five -= 3
+        dic = {5:0, 10:0}
+
+        for bill in bills:
+            if bill == 5:
+                dic[5] += 1
+            elif bill == 10:
+                if dic[5] < 1:
+                    return False
+                dic[5] -= 1
+                dic[10] += 1
             else:
-                return False
+                if dic[10] and dic[5]:
+                    dic[10] -= 1
+                    dic[5] -= 1
+                elif dic[5] >= 3:
+                    dic[5] -= 3
+                else:
+                    return False
         return True
