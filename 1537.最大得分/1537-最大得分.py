fc01070f79c9b859b4913a6662e2421bd9571757
JiayangWu <530081999@qq.com>
2020-08-22 01:05:30 -0400

    2020-08-22

--- 1537.最大得分/1537-最大得分.py
+++ 1537.最大得分/1537-最大得分.py
@@ -5,44 +5,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        from collections import defaultdict
-        from heapq import *
-        num2idx = defaultdict(list)
-        for i, num in enumerate(nums1):
-            num2idx[num] = [i]
+        dups = set(nums1) & set(nums2)
 
-        for i, num in enumerate(nums2):
-            num2idx[num].append(i)
+        s1 = self.getFragmentedSum(nums1, dups)
+        s2 = self.getFragmentedSum(nums2, dups)
 
-        dups = set() 
-        for key, val in num2idx.items():
-            if len(val) > 1:
-                dups.add(key)
-
-        num2sum = defaultdict(int)
         res = 0
-        queue = [(-nums1[-1], len(nums1) - 1, 0, 1), (-nums2[-1], len(nums2) - 1, 0, 2)]
-        heapify(queue)
-        while queue:
-            val, idx, s, flag = heappop(queue)
-            val = -val
-
-            if flag == 1 and idx + 1 < len(nums1) and nums1[idx + 1] in dups:
-                s = max(num2sum[nums1[idx + 1]], s)
-            elif flag == 2 and idx + 1 < len(nums2) and nums2[idx + 1] in dups:
-                s = max(num2sum[nums2[idx + 1]], s)
-
-            if val in dups:
-                s = max(num2sum[val], s + val)
-                num2sum[val] = max(s, num2sum[val])
-            else:
-                s += val 
-
-            if idx:
-                if flag == 1:
-                    heappush(queue, (-nums1[idx - 1], idx - 1, s, 1))
-                else:
-                    heappush(queue, (-nums2[idx - 1], idx - 1, s, 2))
-            res = max(res, s)
+        for sum1, sum2 in zip(s1, s2):
+            res += max(sum1, sum2)
         return res % (10 ** 9 + 7)
-        
+
+    def getFragmentedSum(self, nums, dups):
+        l = []
+        s = 0
+        for num in nums:
+            s += num 
+            if num in dups:
+                l.append(s)
+                s = 0
+        l.append(s)
+        return l
