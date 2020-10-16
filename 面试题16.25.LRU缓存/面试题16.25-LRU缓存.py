719c95796a371542028a1662c1b207f74bcdaee1
JiayangWu <530081999@qq.com>
2020-10-16 01:20:20 -0400

    2020-10-16

--- 面试题16.25.LRU缓存/面试题16.25-LRU缓存.py
+++ 面试题16.25.LRU缓存/面试题16.25-LRU缓存.py
@@ -1,9 +1,9 @@
 class DLLNode(object):
-    def __init__(self, key, val, pre, nxt):
+    def __init__(self, key, val, pre = None, next = None):
         self.key = key
-        self.val = val 
-        self.pre = pre 
-        self.nxt = next
+        self.val = val
+        self.pre = pre
+        self.next = next
 
 class LRUCache(object):
     def __init__(self, capacity):
@@ -12,22 +12,21 @@ class LRUCache(object):
         """
         self.capacity = capacity
         self.size = 0
-        self.head = DLLNode(-1, -1, None, None)
-        self.tail = DLLNode(-1, -1, self.head, None)
-        self.head.next = self.tail 
-        self.dic = dict()
+        self.head = DLLNode(-1, -1)
+        self.tail = DLLNode(-1, -1, self.head)
+        self.head.next = self.tail
+        self.dic = {}
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        if key not in self.dic:
-            return -1
-        else:
-            value = self.dic[key].val
-            self.moveToHead(self.dic[key])
-            return value 
+        if key in self.dic:
+            res = self.dic[key].val
+            self.moveToHead(self.dic[key]) # set this node to be most recently used
+            return res
+        return -1
 
     def put(self, key, value):
         """
@@ -35,40 +34,42 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
-        if key not in self.dic:
-            node = DLLNode(key, value, None, None)
+        if key in self.dic:
+            self.dic[key].val = value
+            self.moveToHead(self.dic[key])
+        else:
+            node = DLLNode(key, value)
             self.dic[key] = node
             if self.size < self.capacity:
                 self.size += 1
             else:
-                self.removeLastElement()
+                self.removeLastNode()
             self.insertToHead(node)
-        else:
-            self.dic[key].val = value
-            self.moveToHead(self.dic[key])
-
+        
     def insertToHead(self, node):
-
         pre_first = self.head.next
         self.head.next = node
-        node.pre = self.head 
-        pre_first.pre = node 
+        node.pre = self.head
         node.next = pre_first
-        
-        # print (node.key, node.pre.key, node.next.key)
+        pre_first.pre = node
 
-    def removeLastElement(self):
-        pre_last = self.tail.pre 
-        pre_second_last = pre_last.pre
-        pre_second_last.next = self.tail
-        self.tail.pre = pre_second_last
-        self.dic.pop(pre_last.key)
-    
     def moveToHead(self, node):
+        # 1. take it out
         node.pre.next = node.next
         node.next.pre = node.pre
-
+        # 2. insert it to the head
         self.insertToHead(node)
+
+    def removeLastNode(self):
+        pre_last = self.tail.pre
+        pre_second_last = pre_last.pre
+
+        pre_second_last.next = self.tail
+        self.tail.pre = pre_second_last
+
+        self.dic.pop(pre_last.key)
+                
+
 
 
 # Your LRUCache object will be instantiated and called as such:
